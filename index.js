9d3faf2574ce777d806295614bdd404116ffcae4
Paras Bukhari <152764697+syedaparas7@users.noreply.github.com>
2025-03-22 00:41:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
-import express from 'express' 
-import cors from 'cors' 
-import authRouter from './routes/auth.js'
-import projectRouter from './routes/project.js'
-import employeeRouter from './routes/employee.js'
+import express from 'express';
+import cors from 'cors';
+import authRouter from './routes/auth.js';
+import projectRouter from './routes/project.js';
+import employeeRouter from './routes/employee.js';
 import userRoutes from './routes/userRoutes.js';
+import connectToDatabase from './db/db.js';
 
+connectToDatabase();
 
-import connectToDatabase from './db/db.js'
+const app = express();
 
-connectToDatabase()
-
-
-const app = express() 
-  const corsOptions = {
-  origin: 'https://deply-project-hzdttathg-deployement.vercel.app/',
+const corsOptions = {
+  origin: 'https://deply-project.vercel.app',
   methods: 'GET,POST,PUT,DELETE',
   credentials: true, 
 };
+
 app.use(cors(corsOptions));
-app.use(express.json()) 
-app.use('/api/auth', authRouter)
-app.use('/api/projects', projectRouter)
-app.use('/api/employee', employeeRouter)
+app.use(express.json());
+
+app.use('/api/auth', authRouter);
+app.use('/api/projects', projectRouter);
+app.use('/api/employee', employeeRouter);
 app.use('/api/users', userRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Hello Slesh');  // 
+});
+
+app.get('/get', (req, res) => {
+  res.send('Hello World backend checkout');
+});
 
 
-app.get('/get',(req,res)=>{
-  res.send('Hello World')
-})
-app.listen(process.env.PORT ||5000, () => { 
-  console.log(`Server is Running on port ${process.env.PORT}`)
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => { 
+  console.log(`Server is Running on port ${PORT}`);
+});
